d66aa982a66133402e95e49e2809848ec4e805d0
shreejadhav10 <shree@3391>
2018-04-19 11:17:32 +0530

    Log commit

--- app/src/main/java/com/example/shree/gitadroidstudiodemo/MainActivity.java
+++ app/src/main/java/com/example/shree/gitadroidstudiodemo/MainActivity.java
@@ -2,8 +2,10 @@ package com.example.shree.gitadroidstudiodemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +18,6 @@ public class MainActivity extends AppCompatActivity {
      * Added by shree
      */
     private void initView() {
-
+        Log.d(TAG, "initView: ");
     }
 }
